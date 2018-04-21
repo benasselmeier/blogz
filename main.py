a6519d8edf5c86545abfc55ded502ce098e8cb00
Ben Asselmeier <benasselmeier@gmail.com>
2018-04-21 11:30:11 -0500

    transferring to desktop

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exists
 
+# stopped at newpost step for the night
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -14,10 +16,26 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     entry = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name, entry):
+    def __init__(self, name, entry, author):
         self.name = name
         self.entry = entry
+        self.author = author
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(20))
+    blogs = db.relationship('Blog', backref='author')
+       
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/', methods=['POST', 'GET'])
