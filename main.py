b6b04e171193909c6dcc1b498b3a1447765bca6f
Ben <benasselmeier@gmail.com>
2018-04-21 14:31:42 -0500

    Sign up page is working correctly with a few minor issues I'll ask about on Monday.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exists
 
-# stopped at newpost step for the night
+# THINGS TO ASK ABOUT:
+# Sign Up function: will show "duplicate user" error, but after showing "invalid username" error, won't go back to showing "duplicate user"
+# under the correct conditions.
+#
+#
+#
+#
+#
+#
 
 
 app = Flask(__name__)
@@ -56,9 +64,61 @@ def display_blog():
             return redirect('/blog')
         blog_entries = [blog_entry]
     return render_template('blog.html', title="Build-a-Blog!", blog_entries=blog_entries)
-        
     
+# TODO: Sign Up Function:
+# User enters a valid new username and password, and is redirected to the /newpost page with their username stored in a session.
+# User gets an error message if:
+#   username, password, or verify forms are blank, invalid, or passwords don't match
+#   username already exists in the database
 
+@app.route('/signup', methods=['POST', 'GET'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        username_error = " "
+        password_error = " "
+        verify_error = " "
+
+        if len(username) >= 3 and " " not in username:
+            username = username
+        else:
+            username = " "
+            username_error = "Invalid username!"
+
+        if len(password) >= 3 and " " not in password and password == verify:
+            password = password
+        else:
+            password = " "
+            password_error = "Invalid password!"
+
+        if verify == password:
+            verify = verify
+        elif verify != password and password == " ":
+            verify = " "
+        else:
+            verify_error = "Passwords don't match!"
+
+        rules = (username_error == " ", password_error == " ", verify_error == " ")
+
+        if all(rules):
+            existing_user = User.query.filter_by(username=username).first()
+            if not existing_user:
+                new_user = User(username, password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                print("it's a new user!")
+                return redirect('/newpost')
+            else:
+                print("Duplicate user")
+                duplicate_error = "Duplicate user!"
+                return render_template('signup.html', duplicate_error=duplicate_error)
+        else:
+            return render_template('signup.html', username=username, username_error=username_error, password_error=password_error, verify_error=verify_error)
+
+    return render_template('signup.html')
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
